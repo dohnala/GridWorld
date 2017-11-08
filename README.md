34ba558ae590ff977dc15e08b9ae223af20240eb
dohnal <dohnal@apitea.com>
2017-11-08 16:51:25 +0100

    Add experiment for DQN agent and find_treasure_v0 task Update README

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 ## Grid World
 
-### random_find_treasure_v0
+### Random agent for find_treasure_v0
 ```
 python3 -m experiments.random_find_treasure_v0 --train=100 --eval=10
+```
+
+### DQN agent for find_treasure_v0
+```
+python3 -m experiments.dqn_find_treasure_v0 --train=300 --eval=30
 ```
---
+++ experiments/dqn_find_treasure_v0.py
@@ -0,0 +1,27 @@
+from agent.dqn_agent import DQNAgent
+from agent.encoder import OneHotEncoder
+from agent.model import NNModel
+from agent.policy import EpsilonGreedyPolicy
+from experiments.experiment import Experiment
+
+
+class FindTreasureV0(Experiment):
+    """
+    Experiment of DQN agent for find_treasure_v0 task.
+    """
+
+    def __init__(self):
+        super(FindTreasureV0, self).__init__("find_treasure_v0")
+
+    def create_agent(self, env):
+        encoder = OneHotEncoder(env.width, env.height)
+        model = NNModel(encoder, env.num_actions, hidden_units=[16])
+
+        return DQNAgent(env, model,
+                        learning_rate=0.01,
+                        discount=0.9,
+                        exploration_policy=EpsilonGreedyPolicy(0.5, 0.01, 100))
+
+
+if __name__ == "__main__":
+    FindTreasureV0().run()
