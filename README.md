fefebea1a13236526d47c967faf375c562c0675a
dohnal <dohnal@apitea.com>
2017-11-09 13:44:29 +0100

    Add dqn_find_treasure_v1 experiment

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 ```
 python3 -m experiments.dqn_find_treasure_v0 --train=300 --eval=30
 ```
+
+### DQN agent for find_treasure_v1
+```
+python3 -m experiments.dqn_find_treasure_v1 --train=3000 --eval=100 --log_every=100
+```
---
+++ experiments/dqn_find_treasure_v1.py
@@ -0,0 +1,28 @@
+from agent.dqn_agent import DQNAgent
+from agent.encoder import OneHotEncoder
+from agent.model import NNModel
+from agent.policy import EpsilonGreedyPolicy
+from experiments.experiment import Experiment
+
+
+class FindTreasureV1(Experiment):
+    """
+    Experiment of DQN agent for find_treasure_v0 task.
+    """
+
+    def __init__(self):
+        super(FindTreasureV1, self).__init__("find_treasure_v1")
+
+    def create_agent(self, env):
+        encoder = OneHotEncoder(env.width, env.height, treasure_position=True)
+        model = NNModel(encoder, env.num_actions, hidden_units=[128])
+
+        return DQNAgent(env, model,
+                        learning_rate=0.002,
+                        discount=0.95,
+                        exploration_policy=EpsilonGreedyPolicy(1, 0.01, 2000),
+                        n_step=8)
+
+
+if __name__ == "__main__":
+    FindTreasureV1().run()
