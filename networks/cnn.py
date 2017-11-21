1463470298654ae9a658b43aecfcceb1b694d0c0
dohnal <dohnal.adam@gmail.com>
2017-11-21 23:04:38 +0100

    Compute output shape of flattened convolution layer

--- networks/cnn.py
+++ networks/cnn.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -26,14 +28,29 @@ class CNNModule(NetworkModule):
         width, height, num_layers = input_shape
 
         self.conv1 = nn.Conv2d(in_channels=num_layers, out_channels=16, kernel_size=3)
-        self.nn = NNModule(784, hidden_units)
+        self.nn = NNModule(self.__conv_shape_flatten__(width, height, self.conv1), hidden_units)
 
     def forward(self, states):
         result = F.relu(self.conv1(states))
         result = result.view(result.size(0), -1)
-        result = self.nn(result)
+        result = self.nn.forward(result)
 
         return result
 
     def output_shape(self):
         return self.nn.output_shape()
+
+    @staticmethod
+    def __conv_shape_flatten__(w_in, h_in, conv):
+        """
+        Return flattened output shape of given convolution layer.
+
+        :param w_in: input width
+        :param h_in: input height
+        :param conv: convolution layer
+        :return: output shape of given convolution layer flattened
+        """
+        w_out = int((w_in + 2 * conv.padding[0] - conv.kernel_size[0]) / conv.stride[0]) + 1
+        h_out = int((h_in + 2 * conv.padding[1] - conv.kernel_size[1]) / conv.stride[1]) + 1
+
+        return w_out * h_out * conv.out_channels
