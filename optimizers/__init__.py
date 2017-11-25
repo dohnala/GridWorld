0014ffa93e3053a49155497f21b1dde6b0fa1230
dohnal <dohnal.adam@gmail.com>
2017-11-25 22:17:35 +0100

    Add Shared Adam optimizer

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -1,7 +1,9 @@
 from optimizers.optimizer import Optimizer
 from optimizers.adam_opimizer import AdamOptimizer
+from optimizers.shared_adam_optimizer import SharedAdamOptimizer
 
 __all__ = [
     'Optimizer',
-    'AdamOptimizer'
+    'AdamOptimizer',
+    'SharedAdamOptimizer'
 ]
---
+++ optimizers/shared_adam_optimizer.py
@@ -0,0 +1,97 @@
+import math
+import torch
+import torch.optim as optim
+
+from optimizers import Optimizer
+
+
+class SharedAdam(optim.Adam):
+    """
+    Shared Adam implementation.
+    """
+
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0):
+        super(SharedAdam, self).__init__(params, lr, betas, eps, weight_decay)
+
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                state['step'] = torch.zeros(1)
+                state['exp_avg'] = p.data.new().resize_as_(p.data).zero_()
+                state['exp_avg_sq'] = p.data.new().resize_as_(p.data).zero_()
+
+    def share_memory(self):
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                state['step'].share_memory_()
+                state['exp_avg'].share_memory_()
+                state['exp_avg_sq'].share_memory_()
+
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                state = self.state[p]
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+
+                state['step'] += 1
+
+                if group['weight_decay'] != 0:
+                    grad = grad.add(group['weight_decay'], p.data)
+
+                # Decay the first and second moment running average coefficient
+                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+
+                denom = exp_avg_sq.sqrt().add_(group['eps'])
+
+                bias_correction1 = 1 - beta1 ** state['step'][0]
+                bias_correction2 = 1 - beta2 ** state['step'][0]
+                step_size = group['lr'] * math.sqrt(
+                    bias_correction2) / bias_correction1
+
+                p.data.addcdiv_(-step_size, exp_avg, denom)
+
+        return loss
+
+
+class SharedAdamOptimizer(Optimizer):
+    """
+    Shared Adam optimizer.
+    """
+
+    def __init__(self, learning_rate):
+        super(SharedAdamOptimizer, self).__init__()
+
+        self.learning_rate = learning_rate
+        self.optimizer = None
+
+    def set_parameters(self, parameters):
+        self.optimizer = optim.Adam(parameters, lr=self.learning_rate)
+
+    def step(self, loss):
+        if self.optimizer:
+            # Zero all gradients
+            self.optimizer.zero_grad()
+
+            # Compute all gradients w.r.t given loss
+            loss.backward()
+
+            # Update all variables with computed gradients
+            self.optimizer.step()
+
+    def state_dict(self):
+        return self.optimizer.state_dict() if self.optimizer else {}
+
+    def load_state_dict(self, state_dict):
+        if self.optimizer:
+            self.optimizer.load_state_dict(state_dict)
