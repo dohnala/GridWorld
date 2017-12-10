0806c719135330cc542356d1bf2aca45e5c3277c
dohnal <dohnal.adam@gmail.com>
2017-12-10 14:49:18 +0100

    Add train_start parameter to memory agent Add asserts to memory agent's configuration

--- agents/memory_agent.py
+++ agents/memory_agent.py
@@ -8,7 +8,7 @@ class MemoryAgentConfig(AgentConfig):
     Memory agent's configuration.
     """
 
-    def __init__(self, encoder, optimizer, train_policy, eval_policy, capacity, batch_size):
+    def __init__(self, encoder, optimizer, train_policy, eval_policy, capacity, batch_size, train_start=None):
         """
         Initialize configuration.
 
@@ -18,11 +18,23 @@ class MemoryAgentConfig(AgentConfig):
         :param eval_policy: policy used in evaluation phase
         :param capacity: capacity of the memory
         :param batch_size: batch used to sample transitions from memory
+        :param train_start: after how many steps should training using batch start
         """
         super(MemoryAgentConfig, self).__init__(encoder, optimizer, train_policy, eval_policy)
 
+        assert capacity > 0, "capacity has to be positive"
+        assert batch_size > 0, "batch_size has to be positive"
+        assert capacity >= batch_size, "capacity has to be greater or equals than batch_size"
+
+        if train_start:
+            assert train_start > 0, "train_start has to be positive"
+            assert train_start >= batch_size, "capacity has to be greater or equals than batch_size"
+
         self.capacity = capacity
         self.batch_size = batch_size
+
+        # If train start is not specified, start training after memory is filled
+        self.train_start = train_start if train_start else capacity
 
 
 class MemoryAgent(Agent):
@@ -34,14 +46,15 @@ class MemoryAgent(Agent):
 
         self.capacity = self.config.capacity
         self.batch_size = self.config.batch_size
+        self.train_start = self.config.train_start
         self.memory = ReplayMemory(self.capacity)
 
     def __observe_transition__(self, transition):
         # Add transition into memory
         self.memory.add(transition)
 
-        # If there is enough transitions in memory
-        if self.memory.size() >= self.batch_size:
+        # If train should start
+        if self.step >= self.train_start:
             # Sample batch from memory
             transitions = self.memory.sample(self.batch_size)
 
